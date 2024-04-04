7869411aaf195cc7cad8336169481cba11aa13bd
master <onagu7167@gmal.com>
2024-04-04 19:56:37 +0900

    Proxy advanced

---
+++ src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,25 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.security.PublicKey;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+//    @Autowired
+//    private CallServiceV1 callServiceV1;
+//
+//    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+//        log.info("call external");
+//        this.callServiceV1 = callServiceV1;
+//    }
+//
+//    public void external() {
+//        log.info("call external");
+//        callServiceV1.external();
+//    }
+}
---
+++ src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,46 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import hello.aop.member.annotation.ClassAop;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@SpringBootTest
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); //JDK 동적 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        //JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패, ClassCastException 예외 발생
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+        });
+
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); //CGLIB 동적 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+
+    }
+
+}
